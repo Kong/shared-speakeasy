96f6b55a45c2eef3ee868d88a3f529d2e434162e
slonka <slonka@users.noreply.github.com>
2025-04-24 13:07:42 +0200

    fix test-all target
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- tfbuilder/plan.go
+++ tfbuilder/plan.go
@@ -1,24 +1,24 @@
 package tfbuilder
 
 import (
-    "context"
-    "encoding/json"
-    "fmt"
-    "github.com/hashicorp/terraform-plugin-testing/plancheck"
-    "log"
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 )
 
 type debugPlan struct{}
 
 func (e debugPlan) CheckPlan(ctx context.Context, req plancheck.CheckPlanRequest, resp *plancheck.CheckPlanResponse) {
-    reqPlan, err := json.Marshal(req.Plan)
-    if err != nil {
-        log.Fatalf(fmt.Sprintf("error marshaling plan request: %s", err))
-    }
-    reqPlanStr := string(reqPlan)
-    log.Printf("req.Plan - %s\n", reqPlanStr)
+	reqPlan, err := json.Marshal(req.Plan)
+	if err != nil {
+		log.Fatalf("error marshaling plan request: %s", err)
+	}
+	reqPlanStr := string(reqPlan)
+	log.Printf("req.Plan - %s\n", reqPlanStr)
 }
 
 func DebugPlan() plancheck.PlanCheck {
-    return debugPlan{}
+	return debugPlan{}
 }

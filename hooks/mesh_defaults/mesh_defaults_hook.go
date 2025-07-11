ee85032332419ff926fbfe90fe00eb32369d3f74
slonka <slonka@users.noreply.github.com>
2025-07-11 16:06:49 +0200

    feat(mesh-defaults): pass functions to mesh-default hooks function
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- hooks/mesh_defaults/mesh_defaults_hook.go
+++ hooks/mesh_defaults/mesh_defaults_hook.go
@@ -5,15 +5,14 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
-	"regexp"
 )
 
 // BeforeRequest defaults mesh related fields:
 // for mesh control plane - the "features" field to include "MeshCreation" and "HostnameGeneratorCreation" with "enabled" set to false
 // for mesh - the "skipCreatingInitialPolicies" field to include "*".
-func BeforeRequest(apiPrefix string, features bool, initialPolicies bool) func(req *http.Request) (*http.Request, error) {
+func BeforeRequest(matchFeatureRequest func(r *http.Request) bool, matchPoliciesRequest func(r *http.Request) bool) func(req *http.Request) (*http.Request, error) {
 	return func(req *http.Request) (*http.Request, error) {
-		if features && req.Method == http.MethodPost && req.URL.Path == apiPrefix {
+		if req.Method == http.MethodPost && matchFeatureRequest(req) {
 			var bodyMap map[string]interface{}
 
 			if req.Body != nil {
@@ -60,48 +59,40 @@ func BeforeRequest(apiPrefix string, features bool, initialPolicies bool) func(r
 			req.Header.Set("Content-Type", "application/json")
 		}
 
-		if initialPolicies {
-			// Check for requests matching POST: apiPrefix/*/api/meshes/*
-			match, err := regexp.MatchString(`^`+apiPrefix+`/meshes/[^/]+$`, req.URL.Path)
+		if matchPoliciesRequest(req) {
+			var bodyMap map[string]interface{}
+
+			if req.Body != nil {
+				bodyBytes, err := io.ReadAll(req.Body)
+				if err != nil {
+					return nil, err
+				}
+				defer req.Body.Close()
+
+				if len(bodyBytes) > 0 {
+					if err := json.Unmarshal(bodyBytes, &bodyMap); err != nil {
+						return nil, err
+					}
+				}
+			}
+
+			if bodyMap == nil {
+				bodyMap = make(map[string]interface{})
+			}
+
+			// Define the default "skipCreatingInitialPolicies" value
+			if _, exists := bodyMap["skipCreatingInitialPolicies"]; !exists {
+				bodyMap["skipCreatingInitialPolicies"] = []string{"*"}
+			}
+
+			newBody, err := json.Marshal(bodyMap)
 			if err != nil {
 				return nil, err
 			}
 
-			if req.Method == http.MethodPut && match {
-				var bodyMap map[string]interface{}
-
-				if req.Body != nil {
-					bodyBytes, err := io.ReadAll(req.Body)
-					if err != nil {
-						return nil, err
-					}
-					defer req.Body.Close()
-
-					if len(bodyBytes) > 0 {
-						if err := json.Unmarshal(bodyBytes, &bodyMap); err != nil {
-							return nil, err
-						}
-					}
-				}
-
-				if bodyMap == nil {
-					bodyMap = make(map[string]interface{})
-				}
-
-				// Define the default "skipCreatingInitialPolicies" value
-				if _, exists := bodyMap["skipCreatingInitialPolicies"]; !exists {
-					bodyMap["skipCreatingInitialPolicies"] = []string{"*"}
-				}
-
-				newBody, err := json.Marshal(bodyMap)
-				if err != nil {
-					return nil, err
-				}
-
-				req.Body = io.NopCloser(bytes.NewReader(newBody))
-				req.ContentLength = int64(len(newBody))
-				req.Header.Set("Content-Type", "application/json")
-			}
+			req.Body = io.NopCloser(bytes.NewReader(newBody))
+			req.ContentLength = int64(len(newBody))
+			req.Header.Set("Content-Type", "application/json")
 
 		}
 		return req, nil

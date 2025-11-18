4b7bc7ddafcfdc181b5500df0cc4bd299d9cb688
slonka <slonka@users.noreply.github.com>
2025-11-18 05:30:46 +0100

    feat(hclbuilder): make tests pass
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- hclbuilder/testdata/nested_block.tf
+++ hclbuilder/testdata/nested_block.tf
@@ -1,7 +1,7 @@
 resource "kong-mesh_mesh_traffic_permission" "allow_all" {
   mesh = "kong-mesh_mesh.default.name"
   name = "allow-all"
-  spec {
+  spec = {
     from = [{
       target_ref = {
         kind = "Mesh"
